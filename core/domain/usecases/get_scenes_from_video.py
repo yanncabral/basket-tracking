a851eb577450ac62152a81d8eab38d5709c53f65
Yann <iamyanndias@gmail.com>
2022-06-19 01:56:02 -0300

    Add `region_of_interest` optional parameter in getScenesFromVideo usecase.

--- core/domain/usecases/get_scenes_from_video.py
+++ core/domain/usecases/get_scenes_from_video.py
@@ -1,13 +1,14 @@
 
 from abc import ABC, abstractmethod
 from pathlib import Path
-from typing import Iterable
+from typing import Iterable, Optional
 
 from core.domain.entities.scene import Scene
+from core.infra.scene_objects.corners import Corners
 
 
 class GetScenesFromVideo(ABC):
     
     @abstractmethod
-    def getScenesFromVideo(self, video_path: Path) -> Iterable[Scene]:
+    def getScenesFromVideo(self, video_path: Path, region_of_interest: Optional[Corners]) -> Iterable[Scene]:
         pass
