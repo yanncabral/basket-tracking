e9bfeae4be76d35193b34af23bd96470197b4760
Yann <iamyanndias@gmail.com>
2022-06-19 04:08:41 -0300

    Add `draw` method in entities.

--- core/domain/entities/entity.py
+++ core/domain/entities/entity.py
@@ -1,10 +1,16 @@
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import Tuple
+
+from numpy.typing import ArrayLike
 
 from core.infra.scene_objects.offset import Offset
 
 
 @dataclass
-class Entity:
+class Entity(ABC):
     """Base class for all entities."""
     position: Offset
+
+    @abstractmethod
+    def draw(self, frame: ArrayLike) -> ArrayLike:
+        pass
