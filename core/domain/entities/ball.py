2543c03ab53bbd6815ae502a69591e3785802466
Yann <iamyanndias@gmail.com>
2022-06-19 04:08:13 -0300

    Add `draw` methods to Entities.

--- core/domain/entities/ball.py
+++ core/domain/entities/ball.py
@@ -1,8 +1,13 @@
 from dataclasses import dataclass
-from core.domain.entities.entity import Entity
 
+from numpy.typing import ArrayLike
+from core.domain.entities.entity import Entity
+from core.data.services.default_image_service import DefaultImageService as ImageService
+
+_image_service = ImageService()
 
 @dataclass
 class Ball(Entity):
     """Ball entity."""
-    pass
+    def draw(self, frame: ArrayLike) -> ArrayLike:
+        return _image_service.drawCircleInFrame(frame=frame, center=self.position)
