72df8bf01ebb749906e05733bd2c5717a945702b
Yann <iamyanndias@gmail.com>
2022-06-19 04:09:18 -0300

    Add draw methods in scene.

--- core/domain/entities/scene.py
+++ core/domain/entities/scene.py
@@ -1,7 +1,11 @@
 from dataclasses import dataclass
-from typing import List
+from typing import List, Optional
 
-from core.domain.entities import Player, Ball
+from numpy.typing import ArrayLike
+from core.domain.entities.court import Court
+
+from core.domain.entities.player import Player
+from core.domain.entities.ball import Ball
 
 @dataclass
 class Scene:
@@ -9,3 +13,26 @@ class Scene:
 
     players: List[Player]
     ball: Ball
+    court: Court
+    frame: ArrayLike
+    
+    def draw_players(self, frame: Optional[ArrayLike] = None) -> ArrayLike:
+        if frame is None:
+            frame = self.frame.copy()
+        players_inside_court = (player for player in self.players if self.court.corners.is_point_inside(player.position))
+        for player in players_inside_court:
+            frame = player.draw(frame)
+
+        return frame
+
+    def draw_ball(self, frame: Optional[ArrayLike] = None) -> ArrayLike:
+        if frame is None:
+            frame = self.frame.copy()
+
+        return self.ball.draw(frame)
+
+    def draw_frame(self) -> ArrayLike:
+        frame = self.draw_players()
+        frame = self.draw_ball(frame=frame)
+        
+        return frame
