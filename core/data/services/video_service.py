b78aa558f36d0935d1ec3bac572f567f8281b39d
Yann <iamyanndias@gmail.com>
2022-06-19 01:53:23 -0300

    Implement getScenesFromVideo method.

---
+++ core/data/services/video_service.py
@@ -0,0 +1,28 @@
+import cv2
+
+from typing import Iterable
+from core.domain.entities.scene import Scene
+from core.domain.services.video_service import VideoService
+
+from .finder_service import DefaultFinderService
+
+_finder_service = DefaultFinderService()
+
+
+class DefaultVideoService(VideoService):
+
+    def getScenesFromVideo(self: "DefaultVideoService", video_path: str) -> Iterable[Scene]:
+        cap = cv2.VideoCapture(video_path)
+
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if ret:
+                ball = _finder_service.findBall(frame)
+                players = _finder_service.findPlayers(frame)
+
+                if ball is not None and players is not None:
+                    # Do not yield if no ball are found
+                    yield Scene(players=players, ball=ball)
+
+        cap.release()
+            
