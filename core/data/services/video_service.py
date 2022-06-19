8a023dfbc7c1c25234e937eee63e0d44262ef46c
Yann <iamyanndias@gmail.com>
2022-06-19 02:14:15 -0300

    Use region of iterest in getScenesFromVideo usecase.

--- core/data/services/video_service.py
+++ core/data/services/video_service.py
@@ -1,23 +1,28 @@
 from pathlib import Path
 import cv2
 
-from typing import Iterable
+from typing import Iterable, Optional
 from core.domain.entities.scene import Scene
 from core.domain.services.video_service import VideoService
+from core.infra.scene_objects.corners import Corners
 
 from .finder_service import DefaultFinderService
+from .image_service import DefaultImageService
 
 _finder_service = DefaultFinderService()
+_image_service = DefaultImageService()
 
 
 class DefaultVideoService(VideoService):
 
-    def getScenesFromVideo(self: "DefaultVideoService", video_path: Path) -> Iterable[Scene]:
+    def getScenesFromVideo(self, video_path: Path, region_of_interest: Optional[Corners]) -> Iterable[Scene]:
         cap = cv2.VideoCapture(str(video_path))
 
         while cap.isOpened():
             ret, frame = cap.read()
             if ret:
+                if region_of_interest is not None:
+                    frame = _image_service.applyMaskFromCorners(frame, region_of_interest)
                 ball = _finder_service.findBall(frame)
                 players = _finder_service.findPlayers(frame)
 
@@ -26,4 +31,3 @@ class DefaultVideoService(VideoService):
                     yield Scene(players=players, ball=ball)
 
         cap.release()
-            
