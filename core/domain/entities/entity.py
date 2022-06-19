5eafd3da0e4242eae7ba0c74e467b53018a284b4
Yann <iamyanndias@gmail.com>
2022-06-19 01:51:54 -0300

    Use Offset instead of Tuple in entity base clas..

--- core/domain/entities/entity.py
+++ core/domain/entities/entity.py
@@ -1,8 +1,10 @@
 from dataclasses import dataclass
 from typing import Tuple
 
+from core.infra.scene_objects.offset import Offset
+
 
 @dataclass
 class Entity:
     """Base class for all entities."""
-    position: Tuple[float, float]
+    position: Offset
