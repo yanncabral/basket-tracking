22b410517d1a6d97174f5024f2fad6a0edbc56ae
Yann <iamyanndias@gmail.com>
2022-06-19 04:10:29 -0300

    Remove unused imports from video service interface.

--- core/domain/services/video_service.py
+++ core/domain/services/video_service.py
@@ -1,11 +1,6 @@
 
-from abc import ABC, abstractmethod
-import pathlib
-from typing import Iterable, Optional
-
-from core.domain.entities.scene import Scene
-from core.domain.usecases.get_scenes_from_video import GetScenesFromVideo
-from core.infra.scene_objects.corners import Corners
+from abc import ABC
+from core.domain.usecases.videos.get_scenes_from_video import GetScenesFromVideo
 
 
 class VideoService(GetScenesFromVideo, ABC):
