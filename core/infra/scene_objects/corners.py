3e385e755e4d78381c1988792c7e764bc97178bb
Yann <iamyanndias@gmail.com>
2022-06-20 04:56:59 -0300

    Format file in a better way.

--- core/infra/scene_objects/corners.py
+++ core/infra/scene_objects/corners.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Tuple
+from typing import Optional, Tuple
 from shapely.geometry import Point, Polygon
 
 from core.infra.scene_objects.offset import Offset
@@ -12,16 +12,33 @@ class Corners:
     bottom_left: Offset
     bottom_right: Offset
 
-    def to_tuple(self) -> Tuple[Tuple[float, float], Tuple[float, float], Tuple[float, float], Tuple[float, float]]:
-        return (self.top_left.to_tuple(), self.top_right.to_tuple(), self.bottom_left.to_tuple(), self.bottom_right.to_tuple())
+    def to_tuple(
+        self,
+    ) -> Tuple[
+        Tuple[float, float],
+        Tuple[float, float],
+        Tuple[float, float],
+        Tuple[float, float],
+    ]:
+        return (
+            self.top_left.to_tuple(),
+            self.top_right.to_tuple(),
+            self.bottom_left.to_tuple(),
+            self.bottom_right.to_tuple(),
+        )
 
     @staticmethod
-    def from_rectangle(top_left_x: float, top_left_y: float, bottom_right_x: float, bottom_right_y: float) -> 'Corners':
+    def from_rectangle(
+        top_left_x: float,
+        top_left_y: float,
+        bottom_right_x: float,
+        bottom_right_y: float,
+    ) -> "Corners":
         return Corners(
             top_left=Offset(x=top_left_x, y=top_left_y),
             top_right=Offset(x=bottom_right_x, y=top_left_y),
             bottom_left=Offset(x=top_left_x, y=bottom_right_y),
-            bottom_right=Offset(x=bottom_right_x, y=bottom_right_y)
+            bottom_right=Offset(x=bottom_right_x, y=bottom_right_y),
         )
 
     def to_polygon(self):
