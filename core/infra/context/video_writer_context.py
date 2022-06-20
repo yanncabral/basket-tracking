56e20c2b5637fa2f4e5801fc3d37a1dd7eb9cac7
Yann <iamyanndias@gmail.com>
2022-06-20 02:47:28 -0300

    Remove unused commented code.

--- core/infra/context/video_writer_context.py
+++ core/infra/context/video_writer_context.py
@@ -35,30 +35,3 @@ class VideoWriterContext:
     def __exit__(self, exc_type, exc_value, exc_traceback):
         if self.writer:
             self.writer.close()
-
-
-# writer = None
-# writer = cv2.VideoWriter("output-vid.mp4", fourcc, 6, (img_dst.shape[1], img_dst.shape[0]), True)
-# points_from_camera = np.array(sorted([[16, 752], [1903, 716], [227, 477], [1641, 456]]))
-
-# frame_num = 0
-# # while frame_num < 10:
-# while cap.isOpened():
-#   ret, frame = cap.read()
-#   if ret == True and frame_num < 30:
-#     print(f"rendering frame {frame_num}")
-#     minimap = pega_a_bola(frame)
-#     if not writer:
-#       writer = cv2.VideoWriter("output-vid.mp4", fourcc, 6, (frame.shape[1], frame.shape[0]), True)
-#     # minimap = getMinimapFromFrame(frame)
-#     # cv2_imshow(minimap)
-#     writer.write(minimap)
-#   if frame_num == 300:
-#     break
-
-#     # break
-#   frame_num += 1
-
-# cap.release()
-# writer.release()
-# del writer
