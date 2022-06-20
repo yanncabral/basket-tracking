ec39c59755d5bf92a2266fc61de92dc9b0439fc3
Yann <iamyanndias@gmail.com>
2022-06-20 05:00:54 -0300

    Add helper operators overload.

--- core/infra/scene_objects/offset.py
+++ core/infra/scene_objects/offset.py
@@ -9,3 +9,18 @@ class Offset:
 
     def to_tuple(self) -> Tuple[float, float]:
         return (self.x, self.y)
+
+    def __add__(self, offset: "Offset") -> "Offset":
+        return Offset(x=self.x+offset.x, y=self.y+offset.y)
+
+    def __sub__(self, offset: "Offset") -> "Offset":
+        return Offset(x=self.x-offset.x, y=self.y-offset.y)
+
+    def __mul__(self, by: "Offset") -> "Offset":
+        return Offset(x=self.x*by, y=self.y*by)
+
+    def __truediv__(self, by: "Offset") -> "Offset":
+        return Offset(x=self.x/by, y=self.y/by)
+
+    def __eq__(self, another: "Offset") -> bool:
+        return another is Offset and (self.x, self.y) == (another.x, another.y)
